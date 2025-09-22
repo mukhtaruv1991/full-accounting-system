0197abe8d431a84109c3308cda6f8de2bc753477
mukhtaruv1991 <amtiaz1991@gmail.com>
2025-09-23 02:34:19 +0300

    slsks

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,21 +8,24 @@ module.exports = {
         ...webpackConfig.resolve.fallback,
         "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
-        "assert": require.resolve("assert"),
+        "assert": require.resolve("assert/"),
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify"),
-        "url": require.resolve("url"),
-        "buffer": require.resolve("buffer")
+        "os": require.resolve("os-browserify/browser"),
+        "url": require.resolve("url/"),
+        "buffer": require.resolve("buffer/")
       };
       
-      // إضافة الـ ProviderPlugin لتوفير buffer بشكل تلقائي
+      // إضافة الـ ProviderPlugin لتوفير buffer و process بشكل تلقائي
       webpackConfig.plugins = (webpackConfig.plugins || []).concat([
         new webpack.ProvidePlugin({
-          process: 'process/browser',
+          process: 'process/browser.js', // <-- التصحيح هنا بإضافة .js
           Buffer: ['buffer', 'Buffer'],
         }),
       ]);
+
+      // تجاهل التحذيرات المتعلقة بـ source-map-loader
+      webpackConfig.ignoreWarnings = [/Failed to parse source map/];
 
       return webpackConfig;
     },
