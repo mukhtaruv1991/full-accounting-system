f5823d5a8289a15b1ec744470163c210fd9fef8b
mukhtaruv1991 <amtiaz1991@gmail.com>
2025-09-23 20:22:51 +0300

    redown

---
+++ backend/src/companies/companies.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CompaniesService {
+  constructor(private prisma: PrismaService) {}
+
+  // Basic service methods can be added here later
+}
